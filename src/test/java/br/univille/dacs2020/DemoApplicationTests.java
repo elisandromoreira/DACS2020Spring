189f9afd50d6657d9dfc8beb795898b05e85c7ac
ELISANDRO DA SILVA MOREIRA <elisandro.moreira@univille.br>
2020-07-09 23:40:18 +0000

    Project Created

---
+++ src/test/java/br/univille/dacs2020/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.dacs2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
